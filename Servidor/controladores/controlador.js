883919cf69665345ba5f11faa6deb10f6675f4a7
Jorge Roldán <roldan21@gmail.com>
2018-12-23 21:08:20 -0600

    eliminación votos

--- Servidor/controladores/controlador.js
+++ Servidor/controladores/controlador.js
@@ -10,7 +10,115 @@ const controlador = {
             }
             res.send(JSON.stringify(resultado));
         });
-    },
+    }, 
+    nuevaCompetencia: (req,res)=>{
+        const nombreCompetencia = req.body.nombre,
+        genero_id = req.body.genero,
+        director_id = req.body.director,
+        actor_id = req.body.actor,
+        anio = parseInt(req.body.anio);
+
+        if (!nombreCompetencia) {
+            console.log("Debe completar el nombre de la competencia");
+            return res.status(422).send("Debe completar el nombre de la competencia");    
+        }
+    
+        let sql = "SELECT * FROM competencia WHERE nombre = '" + nombreCompetencia + "'";
+    
+        con_db.query(sql, (error, resultado)=> {
+            if (error) {
+                console.log("Hubo un error en la consulta", error.message);
+                return res.status(404).send("Hubo un error en la consulta");
+            }
+    
+            if (resultado.length === 1) {
+                console.log("Ya hay una competencia con este nomnbre");
+                return res.status(422).send("Ya hay una competencia con este nomnbre");
+            }
+    
+            let sql = "SELECT count(pelicula.id) As cantidad FROM pelicula", join = "", where = "", campos = "nombre", valores = ") VALUES ('" + nombreCompetencia + "'";
+    
+            if (genero_id > 0) {
+                if (where.length > 0){
+                    where += " and ";
+                } else {
+                    where += " WHERE ";
+                }
+    
+                where += "pelicula.genero_id = " + genero_id;
+                campos += ",genero_id";
+                valores += "," + genero_id;
+            }
+        
+            if (director_id > 0) {
+                join += " INNER JOIN director_pelicula ON pelicula.id = director_pelicula.pelicula_id" ;
+    
+                if (where.length > 0){
+                    where += " and ";
+                } else {
+                    where += " WHERE ";
+                }
+    
+                where +=  "director_pelicula.director_id = " + director_id;
+                campos += ",director_id";
+                valores += "," + director_id;
+            }
+        
+            if (actor_id > 0) {
+                join += " INNER JOIN actor_pelicula ON pelicula.id = actor_pelicula.pelicula_id" ;
+    
+                if (where.length > 0){
+                    where += " and ";
+                } else {
+                    where += " WHERE ";
+                }
+    
+                where += "actor_pelicula.actor_id = " + actor_id;
+                campos += ",actor_id";
+                valores += "," + actor_id;
+            }
+    
+            if (anio > 0) {
+                if (where.length > 0){
+                    where += " and ";
+                } else {
+                    where += " WHERE ";
+                }
+    
+                where += "pelicula.anio = " + anio;
+                campos += ",anio";
+                valores += "," + anio;
+            }
+    
+            sql += join + where;
+            
+            con_db.query(sql, (error, resultado)=> {
+                if (error) {
+                    console.log("Hubo un error en la consulta", error.message);
+                    return res.status(404).send("Hubo un error en la consulta");
+                }
+                
+                if (resultado.length === 0 || resultado[0].cantidad <= 1) {
+                    console.log("Con este criterio no hay 2 peliculas como minimo");
+                    return res.status(422).send("Con este criterio no hay 2 peliculas como minimo");
+                }
+        
+                sql = "INSERT INTO competencia ("+ campos + valores + ")";
+    
+                con_db.query(sql, (error, resultado)=> {
+                    if (error) {
+                        console.log("Hubo un error en la consulta", error.message);
+                        return res.status(404).send("Hubo un error en la consulta");
+                    }
+        
+                    res.status(200).send();    
+                });                
+            });        
+        });
+    }, 
+    eliminarCompetencias: (req, res)=>{
+        console.log(`falta construir`)
+    }, 
     obtenerDosPelisRandom: (req, res) => {
         let idCompetencia = req.params.id
         const sqlCompetencia = `select id from competencia where id = ${idCompetencia}`
@@ -56,6 +164,32 @@ const controlador = {
                 return res.status(404).send("Upss la competencia que intenta obtener no existe");
             }
         })
+    },
+    eliminarVotos: (req,res)=>{
+        const idCompetencia = req.params.id; 
+        console.log(idCompetencia)
+        let sql = "SELECT * FROM competencia WHERE id = " + idCompetencia;
+        
+        con_db.query(sql, (error, resultado)=> {
+            if (error) {
+                console.log("Hubo un error en la consulta", error.message);
+                return res.status(404).send("Hubo un error en la consulta");
+            }
+            if (resultado.length === 0) {
+                console.log("No se encontro ninguna competencia con este id");
+                return res.status(404).send("No se encontro ninguna competencia con este id");
+            }
+    
+            let sql = "DELETE FROM voto WHERE voto.competencia_id = " + idCompetencia;
+    
+            con_db.query(sql, (error, resultado)=> {
+                if (error) {
+                    console.log("Hubo un error en la eliminacion de los votos", error.message);
+                    return res.status(500).send("Hubo un error en la eliminacion de los votos");
+                }
+                res.status(200).send();    
+            });             
+        });
     },
     buscarResultados: (req, res) => {
         const idCompetencia = req.params.id; 
--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -19,15 +19,19 @@ app.use(bodyParser.json());
 //Consultas archivos desde el servidor
 app.use('/', express.static(path.join(__dirname, '/../cliente')));
 app.use('/', express.static(path.join(__dirname, '/../cliente/html')))
+app.use('/', express.static(path.join(__dirname, '/../cliente/html/administrar')))
 
 //rutas
 app.get('/actores', controlador.buscarActor)
 app.get('/directores', controlador.buscarDirector)
 app.get('/generos', controlador.buscarGenero)
 app.get('/competencias', controlador.listarCompetencias);
-app.post('/competencias/:id/voto', controlador.guardarVotos)
+app.post('/competencias', controlador.nuevaCompetencia);
 app.get('/competencias/:id/peliculas', controlador.obtenerDosPelisRandom);
-app.get('/competencias/:id/resultados', controlador.buscarResultados)
+app.get('/competencias/:id/resultados', controlador.buscarResultados);
+app.delete('/competencias/:id/votos', controlador.eliminarVotos);
+app.post('/competencias/:id/voto', controlador.guardarVotos);
+
 
 
 //seteamos el puerto en el cual va a escuchar los pedidos la aplicación
